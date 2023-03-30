e703bb0a3f1d635c81669df4cfdcb09992690338
Lukas <lukasstrassel@googlemail.com>
2023-03-30 09:53:39 +0200

    fix: refrence code

--- src/interfaces/IStaticATokenLM.sol
+++ src/interfaces/IStaticATokenLM.sol
@@ -247,6 +247,7 @@ interface IStaticATokenLM is IInitializableStaticATokenLM {
    * through a redeem call to the aToken underlying.
    * While redeem of aToken is not affected by aave pool configrations, redeeming of the aTokenUnderlying will need to redeem from aave
    * so it is affected by current aave pool configuration.
+   * Reference: https://github.com/aave/aave-v3-core/blob/29ff9b9f89af7cd8255231bc5faf26c3ce0fb7ce/contracts/protocol/libraries/logic/ValidationLogic.sol#L87
    * - MUST return a limited value if owner is subject to some withdrawal limit or timelock.
    * - MUST return balanceOf(owner) if owner is not subject to any withdrawal limit or timelock.
    * - MUST NOT revert.
