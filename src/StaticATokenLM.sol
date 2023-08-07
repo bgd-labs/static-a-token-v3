28a3794e48c71a40065465763fabc5c40260cd26
Lukas <lukasstrassel@googlemail.com>
2023-08-07 12:37:26 +0200

    fix: typo error message

--- src/StaticATokenLM.sol
+++ src/StaticATokenLM.sol
@@ -456,10 +456,10 @@ contract StaticATokenLM is
     uint256 assets = _assets;
     uint256 shares = _shares;
     if (shares > 0) {
-      require(shares <= maxMint(receiver), 'ERC4626: redeem more than max');
+      require(shares <= maxMint(receiver), 'ERC4626: mint more than max');
       assets = previewMint(shares);
     } else {
-      require(assets <= maxDeposit(receiver), 'ERC4626: withdraw more than max');
+      require(assets <= maxDeposit(receiver), 'ERC4626: deposit more than max');
       shares = previewDeposit(assets);
     }
     require(shares != 0, StaticATokenErrors.INVALID_ZERO_AMOUNT);
