475d9a25de4e1ece0e086cdb620e0dd5735e1572
sakulstra <lukasstrassel@googlemail.com>
2024-03-28 14:43:04 +0100

    feat: add aggregator oracle

---
+++ tests/StataOracle.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import 'forge-std/Test.sol';
+import {AaveV3Avalanche, IPool, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
+import {StaticATokenLM, IERC20, IERC20Metadata, ERC20} from '../src/StaticATokenLM.sol';
+import {StataOracle} from '../src/StataOracle.sol';
+import {RayMathExplicitRounding, Rounding} from '../src/RayMathExplicitRounding.sol';
+import {IStaticATokenLM} from '../src/interfaces/IStaticATokenLM.sol';
+import {BaseTest} from './TestBase.sol';
+
+contract StataOracleTest is BaseTest {
+  using RayMathExplicitRounding for uint256;
+
+  address public constant override UNDERLYING = AaveV3AvalancheAssets.WETHe_UNDERLYING;
+  address public constant override A_TOKEN = AaveV3AvalancheAssets.WETHe_A_TOKEN;
+  address public constant EMISSION_ADMIN = 0xCba0B614f13eCdd98B8C0026fcAD11cec8Eb4343;
+
+  IPool public override pool = IPool(AaveV3Avalanche.POOL);
+  StataOracle public oracle;
+
+  function setUp() public override {
+    vm.createSelectFork(vm.rpcUrl('avalanche'), 38011791);
+    super.setUp();
+    oracle = new StataOracle(AaveV3Avalanche.POOL_ADDRESSES_PROVIDER);
+  }
+
+  function test_oraclePrice() public {
+    uint256 stataPrice = oracle.getAssetPrice(address(staticATokenLM));
+    uint256 underlyingPrice = AaveV3Avalanche.ORACLE.getAssetPrice(UNDERLYING);
+    assertGt(stataPrice, underlyingPrice);
+    assertApproxEqAbs(
+      stataPrice,
+      underlyingPrice,
+      ((underlyingPrice * AaveV3Avalanche.POOL.getReserveNormalizedIncome(UNDERLYING)) / 1e27) -
+        underlyingPrice
+    );
+  }
+}
